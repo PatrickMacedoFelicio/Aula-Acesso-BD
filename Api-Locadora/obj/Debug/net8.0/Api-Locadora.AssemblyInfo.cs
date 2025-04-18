f83aad1716f44f1043abab30df288bc00fc01d33
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2025-04-18 13:06:49 -0400

    a

--- Api-Locadora/obj/Debug/net8.0/Api-Locadora.AssemblyInfo.cs
+++ Api-Locadora/obj/Debug/net8.0/Api-Locadora.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api-Locadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3dde9bc2cde72672c9e5a7b65d31c73b8a5772c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afa932dd4531e5249c3f087c5805379cffef1355")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api-Locadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api-Locadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
